a8977bcc786e47cf89c49b7d7b066ad8b959e8bb
Julian-Rueda <jarueda1170@gmail.com>
2021-10-04 21:24:46 -0500

    usuario actualizado

--- src/controlador/Main.java
+++
@@ -1,3 +0,0 @@
-package controlador;
-
-public class Main {
--- src/controlador/Usuario.java
+++ src/controlador/Usuario.java
@@ -1,6 +1,12 @@
 package controlador;
 
 public class Usuario {
+
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
     private int cedula;
     private String nombre =null;
     private int edad;
@@ -8,5 +14,40 @@ public class Usuario {
     private String direccion =null;
     private String correo =null;
 
+    public int getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public int getTelefono(){return telefono;}
+    public String getDireccion(){
+        return direccion;
+    }
+    public String getCorreo(){
+        return correo;
+    }
+
+    public void setNombre(int cedula) {
+        this.cedula = cedula;
+    }
+    public void setEdad(int cedula) {
+        this.cedula = cedula;
+    }
+    public void setTelefono(int cedula) {this.cedula = cedula;}
+    public void setDireccion(int cedula) {
+        this.cedula = cedula;
+    }
+    public void setCorreo(int cedula) {
+        this.cedula = cedula;
+    }
+
+
+
 }
+
 
